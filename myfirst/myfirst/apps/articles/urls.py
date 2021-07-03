dc6f8374a77cbbd5f5020d5ef827d97bf0f5d626
olukh <gabitoff.ar@yandex.ru>
2021-07-03 17:21:13 +0500

    update

--- myfirst/myfirst/apps/articles/urls.py
+++ myfirst/myfirst/apps/articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'articles'
+app_name = 'article'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:article_id>/', views.detail, name='detail'),
